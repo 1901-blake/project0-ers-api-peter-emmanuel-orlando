a7161e10ddc5e16f6922192448f89e3a8488d373
Peter Emmanuel Orlando <peter.emmanuel.orlando@gmail.com>
2019-01-25 14:10:43 -0500

    sucsessfully prototyped method to talk to db

--- src/dbGoBetween.ts
+++ src/dbGoBetween.ts
@@ -1,17 +1,24 @@
-import { Pool, Client } from "pg";
+import { Pool, Client, QueryResult } from "pg";
 import { getSuperSecretPassword } from "./superNormalFile.ignored";
+
 
 const pool = new Pool({
     user: 'p0670083130',
-    host: 'database.server.com',
-    database: 'mydb',
+    host: 'revature-server.cehts5jd7lml.us-east-2.rds.amazonaws.com',
+    database: 'p0670083130',
     password: getSuperSecretPassword(),
-    port: 3211,
+    port: 5432,
   })
-
   
-export function talkToDB( sqlCommand: string)
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
+export function talkToDB( sqlCommand: string, callback:(err: Error, result: QueryResult) => void)
+{
+    let result: QueryResult = undefined;
+    pool.query(sqlCommand, (err, res) => {
+        //console.log(err, res);
+        callback(err, res);
+        pool.end();
+    })
+    return result;
+}
+
+talkToDB('select * from customer',(err, res) =>{ console.log(err, res)});
